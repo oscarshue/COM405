223c8c3fc339050bb399004ff132ed241728a96a
Oscar Shue <4shueo10@solent.ac.uk>
2019-10-29 15:41:29 +0000

    Created car class and added ADD and REMOVE methods to CarPark clas

--- Week6/src/week6/CarPark.java
+++ Week6/src/week6/CarPark.java
@@ -1,5 +1,16 @@
 package week6;
 
 public class CarPark {
+    private int carParkSpaces = 15;
+    private int totalCars;
     
+    public void AddCar()
+    {
+        
+    }
+    
+    public void RemoveCar()
+    {
+    
+    }
 }
