d73e8d9a2a479132c5cc534ada68bfe33302f1d5
Oscar Shue <4shueo10@solent.ac.uk>
2019-10-29 15:23:24 +0000

    Added console instructions

---
+++ Week6/src/week6/Week6.java
@@ -0,0 +1,12 @@
+package week6;
+
+public class Week6 {
+
+    public static void main(String[] args) {
+        System.out.println("Please select one of the following options:");
+        System.out.println("A - Add a car");
+        System.out.println("B - Remove a car");
+        System.out.println("C - View number of free spaces/number of cars in the car park");
+        System.out.println("X - Quit");
+    }    
+}
