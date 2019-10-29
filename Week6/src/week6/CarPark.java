dd023ff0ad8bf49ecc63ed3a4a90868ebceb2b71
Oscar Shue <4shueo10@solent.ac.uk>
2019-10-29 16:24:50 +0000

    Finalised Main method interaction with CarPark class

--- Week6/src/week6/CarPark.java
+++ Week6/src/week6/CarPark.java
@@ -2,28 +2,32 @@ package week6;
 
 public class CarPark {
     private int maxCarParkSpaces = 15;
-    private int totalCars;
-    private int remainingSpaces;
+    private int totalCars = 0;
+    private int remainingSpaces = 15;
     
-    public void AddCar()
-    {
-        if(maxCarParkSpaces > remainingSpaces)
-        {
+    public void AddCar() {
+        if (totalCars < maxCarParkSpaces) {
             // Enter code here to input car registration here
             totalCars++;
             remainingSpaces = maxCarParkSpaces - totalCars;
-            System.out.println("There are " + remainingSpaces + " spaces left.");
-        } else 
-        {
+            System.out.println("Car Added; There are " + remainingSpaces + " spaces left.");
+        } else {
             System.out.println("The car park is already full");
         }
     }
-    
-    public void RemoveCar()
-    {
+
+    public void RemoveCar() {
         // Enter code here to input car registration
-        totalCars--;
-        remainingSpaces = maxCarParkSpaces - totalCars;
-        System.out.println("There are " + remainingSpaces + " spaces left.");
+        if (totalCars > 0) {
+            totalCars--;
+            remainingSpaces = maxCarParkSpaces - totalCars;
+            System.out.println("There are " + remainingSpaces + " spaces left.");
+        } else {
+            System.out.println("There are no cars to remove.");
+        }
+    }
+
+    public int GetRemainingSpaces() {
+        return remainingSpaces;
     }
 }
--- Week6/src/week6/Week6.java
+++ Week6/src/week6/Week6.java
@@ -1,31 +1,33 @@
 package week6;
+
 import java.util.Scanner;
 
 public class Week6 {
 
     public static void main(String[] args) {
-        System.out.println("Please select one of the following options:");
-        System.out.println("A - Add a car");
-        System.out.println("B - Remove a car");
-        System.out.println("C - View number of free spaces/number of cars in the car park");
-        System.out.println("X - Quit");
-        
         Scanner scanIn = new Scanner(System.in);
-        String userInput = scanIn.nextLine().toUpperCase();
+        String userInput;
+        CarPark carPark = new CarPark();
+        
+        do {
+            System.out.println("Please select one of the following options:");
+            System.out.println("A - Add a car");
+            System.out.println("B - Remove a car");
+            System.out.println("C - View number of free spaces");
+            System.out.println("X - Quit");
+            userInput = scanIn.nextLine().toUpperCase();
+            
+            if (userInput.equals("A")) {
+                carPark.AddCar();
+            } else if (userInput.equals("B")) {
+                carPark.RemoveCar();
+            } else if (userInput.equals("C")) {
+                System.out.println(carPark.GetRemainingSpaces() + " remaining spaces.");
+            } else if (userInput.equals("X")) {
+                System.out.println("Application closing");
+            }
+        } while (!userInput.equals("X"));
+
         scanIn.close();
-        
-        if(userInput.equals("A"))
-        {
-        
-        } else if(userInput.equals("B"))
-        {
-        
-        } else if(userInput.equals("C"))
-        {
-        
-        } else if(userInput.equals("X"))
-        {
-        
-        }
-    }    
+    }
 }
