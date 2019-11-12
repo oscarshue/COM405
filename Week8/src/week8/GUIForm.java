c2a1e658991b4bad128da8e3f9aa99bfe9131e32
Oscar Shue <4shueo10@solent.ac.uk>
2019-11-12 15:25:08 +0000

    Added initial components

---
+++ Week8/src/week8/GUIForm.java
@@ -0,0 +1,21 @@
+package week8;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class GUIForm {
+    private JLabel lblReg;
+    private JLabel lblColour;
+    private JLabel lblMake;
+    private JLabel lblModel;
+    private JLabel lblStatus;
+    
+    private JTextField txtReg;
+    private JTextField txtColour;
+    private JTextField txtMake;
+    private JTextField txtModel;
+    
+    private JButton btnAdd;
+    private JButton btnRemove;
+}
