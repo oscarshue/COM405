6f3815163f7d3e99a9ca24d85877a7186f739944
Oscar Shue <4shueo10@solent.ac.uk>
2019-12-10 15:23:22 +0000

    Added week 12 initial classes

---
+++ week12/src/week12/Week12.java
@@ -0,0 +1,21 @@
+package week12;
+
+public class Week12 {
+
+    // Task:
+//Create a system that represents cards in a deck.
+//The cards should have a number and a suit.
+//The Deck class will hold the cards in a LinkedList.
+//The Deck class should allow a card to be added to the Deck - when added, the card should be placed at the end of the deck.
+//The class should also allow you to remove a card. The removed card should be taken from the top of the deck and returned by the method.
+//        
+//Further task
+//Add a shuffle method to your deck class. This should change the order of the cards in your deck.
+//How would you go about this?
+//Hint. it will require a random number generator
+    
+    public static void main(String[] args) {
+        // Week 12 - Task 3 - Linked List
+    }
+    
+}
